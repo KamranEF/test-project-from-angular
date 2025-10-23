741bdd5b473920702e07fcfe1d787fdb5a47dc78
Eminov Kamran <eminovkamran07@gmail.com>
2025-10-23 17:12:06 +0300

    F-1-Review

--- src/app/shared/components/movie-card/movie-card.component.ts
+++ src/app/shared/components/movie-card/movie-card.component.ts
@@ -1,7 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzTagComponent } from 'ng-zorro-antd/tag';
+import { MovieDetailsRoot } from '../../../core/services/types/movie-details.type';
+import { MovieDetailsCard } from '../movie-details-card/movie-details-card.components';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { MovieService } from '../../../core/services/service/movie.service';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-movie-card',
@@ -17,5 +22,35 @@ export class MovieCard {
   @Input() posterPath = '';
   @Input() voteAverage: number | null = null;
   @Input() releaseDate = '';
-  @Input() viewMovieDetails!: (id: number | null) => void;
+  private MovieService = inject(MovieService);
+  private ModalService = inject(NzModalService);
+
+  public viewMovieDetails(id: number | null) {
+    this.MovieService.getMovieById(id)
+      .pipe(
+        catchError(() => {
+          return of(null);
+        }),
+      )
+      .subscribe((movieDetails) => {
+        if (movieDetails) {
+          this.openMovieDetailsModal(movieDetails);
+        }
+      });
+  }
+
+  public openMovieDetailsModal(movie: MovieDetailsRoot) {
+    this.ModalService.create({
+      nzTitle: '',
+      nzFooter: null,
+      nzMask: false,
+      nzClosable: true,
+      nzMaskClosable: true,
+      nzContent: MovieDetailsCard,
+      nzClassName: 'movie-details-card',
+      nzData: {
+        movie: movie,
+      },
+    });
+  }
 }
